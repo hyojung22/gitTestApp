58f29b046e864a264e8fbf862424bb89640fd5e9
hbk <136784349+habyeongkwon@users.noreply.github.com>
2023-08-04 17:51:21 +0900

    도서 완료

--- ddrg/src/pages/M_book.jsx
+++ ddrg/src/pages/M_book.jsx
@@ -7,6 +7,7 @@ const M_book = () => {
 
   let randomPick = [~~(Math.random()*20)] // 20권 중에
   let [category, setCategory] = useState('종합');
+  let [cateplus, setCateplus] = useState(0);
 
   const bestBook = () =>{
     randomPick=[~~(Math.random()*20)];
@@ -23,28 +24,28 @@ const M_book = () => {
   }
   bestBook();
 
-  const cellectHandle = (category) =>{
+  const cellectHandle = (category,num) =>{
     setCategory(category)
+    setCateplus(num)
     bestBook()
   }
   
-  // console.log(All.제목[randomPick]);
 
   return (
     <div className='center'>
       <div>
-        <button onClick={()=>cellectHandle('종합')}>종합</button>
-        <button onClick={()=>cellectHandle('소설')}>소설</button>
-        <button onClick={()=>cellectHandle('에세이')}>에세이</button>
-        <button onClick={()=>cellectHandle('인문')}>인문</button>
-        <button onClick={()=>cellectHandle('경제')}>경제</button>
-        <button onClick={()=>cellectHandle('과학')}>과학</button>
-        <button onClick={()=>cellectHandle('자기계발')}>자기계발</button>
+        <button onClick={()=>cellectHandle('종합',0)}>종합</button>
+        <button onClick={()=>cellectHandle('소설',20)}>소설</button>
+        <button onClick={()=>cellectHandle('에세이',40)}>에세이</button>
+        <button onClick={()=>cellectHandle('인문',60)}>인문</button>
+        <button onClick={()=>cellectHandle('경제',80)}>경제</button>
+        <button onClick={()=>cellectHandle('과학',100)}>과학</button>
+        <button onClick={()=>cellectHandle('자기계발',120)}>자기계발</button>
       </div>
       <div className='bookCase'>
-        <Book All={All} category={category} randomPick={randomPick[0]}></Book>
-        <Book All={All} category={category} randomPick={randomPick[1]}></Book>
-        <Book All={All} category={category} randomPick={randomPick[2]}></Book>
+        <Book All={All} category={category} randomPick={randomPick[0]} cateplus={cateplus}></Book>
+        <Book All={All} category={category} randomPick={randomPick[1]} cateplus={cateplus}></Book>
+        <Book All={All} category={category} randomPick={randomPick[2]} cateplus={cateplus}></Book>
       </div>
     </div>
   )
