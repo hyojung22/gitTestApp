28769134ad251646c5b0e082144eed3dcaa191dc
hyojung22 <95choihyojung@gmail.com>
2023-08-10 11:24:59 +0900

    kakao

--- ddrg/src/components/Book.jsx
+++ ddrg/src/components/Book.jsx
@@ -2,14 +2,16 @@ import React from 'react'
 
 const book = ({All, category, randomPick, cateplus}) => {
 
+
+  
   const imageSrc = require(`../books/베스트셀러표지/${category}/${category}${~~(randomPick)+1}.jpg`);
 
   console.log(cateplus);
   return (
-    <div className='bookInfo'>
-      <img src={imageSrc} className='bookCover'/>
-      <span>{All.제목[randomPick+cateplus]}</span>
-      <span>{All.저자[randomPick+cateplus]} 지음 | {All.가격[randomPick+cateplus]}</span>
+    <div className='bookInfo'> 
+        <img src={imageSrc} className='bookCover'/>
+        <p className='book_content1'>{All.제목[randomPick+cateplus]}</p>
+        <p className='book_content2'>{All.저자[randomPick+cateplus]} 지음 | {All.가격[randomPick+cateplus]}</p>
     </div>
   )
 }
--- ddrg/src/pages/M_book.jsx
+++ ddrg/src/pages/M_book.jsx
@@ -2,11 +2,32 @@ import React, { useState } from 'react'
 import All from '../etc/all.json'
 import Book from '../components/Book.jsx'
 
+import shape_icon from '../img/도서img/circle.png'
+import shape_icon1 from '../img/도서img/Random_img.png'
+import bookImgSrc from '../img/도서img/book_bar.png'
+
 const M_book = () => {
+
+  // const bookImgSrc = require('../img/도서img/book_bar.png') 
+
+  // 점 위치 설정
+  const positions = {
+    '종합' : 139.5,
+    '소설' : 62.5,
+    '에세이' : -21.5,
+    '인문' : -104.5,
+    '경제' : -182.5,
+    '과학' : -259.5,
+    '자기계발' : -349.5,
+  }
 
   let randomPick = [~~(Math.random()*20)] // 20권 중에
   let [category, setCategory] = useState('종합');
   let [cateplus, setCateplus] = useState(0);
+
+  let [showShape, setShowShape] = useState(false); // 도형 이미지 표시 여부 상태 추가
+  let [shapePosition, setShapePosition] = useState(0); // 도형 위치 변경
+
 
   const bestBook = () =>{
     randomPick=[~~(Math.random()*20)];
@@ -27,12 +48,23 @@ const M_book = () => {
     setCategory(category)
     setCateplus(num)
     bestBook()
+
+    setShowShape(true) // 도형 이미지 보이게 설정
+    setShapePosition(positions[category]) // 해당 카테고리에 맞는 위치로 설정
   }
-  
 
   return (
     <div className='center'>
+      <img src={shape_icon1} alt='말풍선 이미지' className='shape_icon1'/>
+      {showShape && <img
+                      src={shape_icon} 
+                      alt='도형 이미지' 
+                      className='shape_icon'
+                      style={{ right : shapePosition + 'px'}}
+                    />
+      }
       <div className='book_btn_box'>
+        <span className='title_heading'>오늘의 PICK</span>
         <button onClick={()=>cellectHandle('종합',0)}>종합</button>
         <button onClick={()=>cellectHandle('소설',20)}>소설</button>
         <button onClick={()=>cellectHandle('에세이',40)}>에세이</button>
@@ -45,7 +77,9 @@ const M_book = () => {
         <Book All={All} category={category} randomPick={randomPick[0]} cateplus={cateplus}></Book>
         <Book All={All} category={category} randomPick={randomPick[1]} cateplus={cateplus}></Book>
         <Book All={All} category={category} randomPick={randomPick[2]} cateplus={cateplus}></Book>
+      
       </div>
+      <img src={bookImgSrc} className='bookBar'/>
     </div>
   )
 }
