10156fcf61aacfd13ed9a237a91c103ebb4f0dbf
lee <greedydog41@gmail.com>
2023-08-11 10:30:49 +0900

    asdfagag

--- ddrg/src/pages/M_news.jsx
+++ ddrg/src/pages/M_news.jsx
@@ -1,13 +1,28 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios'
 const M_news = () => {
-  const [data, setData] = useState([]);
+  const [data1, setData1] = useState([]);
+  const [data2, setData2] = useState([]);
   
   useEffect(() => {
     const fetchData = async () => {
       try {
         const result = await axios.get("http://127.0.0.1:5021/news_list");
-        setData(result.data);
+        setData1(result.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+        // 오류 처리: 사용자에게 오류 메시지를 보여줄 수 있음
+      }
+    };
+    
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const result = await axios.get("http://127.0.0.1:5021/keyword_list");
+        setData2(result.data);
       } catch (error) {
         console.error("Error fetching data:", error);
         // 오류 처리: 사용자에게 오류 메시지를 보여줄 수 있음
@@ -19,7 +34,12 @@ const M_news = () => {
 
   return (
     <ul>
-      {data.map((item) => (
+       {data2.map((item) => (
+          <li>
+            {item.keyword[0]}
+          </li>
+      ))}
+      {data1.map((item) => (
         item.prediction === 1 ? (
           <li key={item.id}>
             {item.content}
